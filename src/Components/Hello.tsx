d59358be6967ffbe08167ceca7aa1be19aa3d52c
unknown <maryemsmadhi@gmail.com>
2024-03-18 06:09:31 +0100

    Add Hello.tsx

---
+++ src/Components/Hello.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Hello(){
+    return(
+        <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content text-center">
+    <div className="max-w-md">
+      <h1 className="text-5xl font-bold">Hello there</h1>
+      
+      
+      <button className="btn btn-primary">Get Started</button>
+    </div>
+  </div>
+</div>
+    )
+}
